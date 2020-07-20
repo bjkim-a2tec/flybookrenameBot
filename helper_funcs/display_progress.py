93e83375a5fc6073b91ae7fed08760f682a93595
bjkim-a2tec <bjkim@a2tec.co.kr>
2020-07-20 19:31:34 +0900

    진행률 한글번역

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -42,14 +42,14 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "[{0}{1}] \nP: {2}%\n".format(
+        progress = "[{0}{1}] \n퍼센트: {2}%\n".format(
             ''.join(["█" for i in range(math.floor(percentage / 5))]),
             ''.join(["░" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "{0} of {1}\nSpeed: {2}/s\nETA: {3}\n".format(
-            humanbytes(current),
+        tmp = progress + "전체{0} 중 {1}\n다운속도: {2}/s\n예상시간: {3}\n".format(
             humanbytes(total),
+            humanbytes(current),
             humanbytes(speed),
             # elapsed_time if elapsed_time != '' else "0 s",
             estimated_total_time if estimated_total_time != '' else "0 s"
